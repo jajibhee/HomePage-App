68d836bb173f1dc5a7356889acc21c74cb38d99a
JAJI <jajmantobi@gmail.com>
2019-03-07 14:49:55 +0100

    mobile view fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,41 @@
 //Grab the element containing the nav icon first
-const menuBtn = document.querySelector(" .mobile_icon");
-const menuItem = document.querySelector(".menu__item");
+const menuBtn = document.querySelector(".menu-mobile_icon");
+const navItem = document.querySelector(".sidebar__nav");
+const list = document.querySelector(".agenda__list");
+const lesson = document.querySelector(".agenda img");
 
-let showNav = false;
-
+//add an event listener on the icon
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if (!showNav) {
-    menuBtn.classList.add("show");
-    menuItem.classList.add("show");
-
-    showNav = true;
-  } else {
-    menuBtn.classList.remove("show");
-    menuItem.classList.remove("show");
-
-    showNav = false;
-  }
+  navItem.classList.toggle("show");
 }
+
+lesson.addEventListener("click", toggleLesson);
+
+function toggleLesson() {
+  list.classList.toggle("show");
+}
+// let myListHeight;
+
+// function toggleLesson() {
+//   if (!list.classList.contains("show")) {
+//     list.classList.add("show");
+//     let listHeight = list.clientHeight;
+//     myListHeight = listHeight;
+//     list.style.height = 0;
+
+//     setTimeout(function() {
+//       list.style.height = listHeight + "px";
+//     }, 100);
+//   } else {
+//     list.classList.remove("show");
+//     setTimeout(function() {
+//       list.style.height = 0 + "px";
+
+//       setTimeout(function() {
+//         list.style.height = myListHeight + "px";
+//       }, 200);
+//     }, 100);
+//   }
+// }
